a2b0ba09a9dae47f10a54814cea67218e2015bee
Hao Xiong <haoxiong@outlook.com>
2017-03-15 21:27:15 -0500

    Initial commit

---
+++ src/Perceptron.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Hao Xiong on 3/16/2017.
+ * Copyright belongs to Hao Xiong, Email: haoxiong@outlook.com
+ */
+
+
+public class Perceptron {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
