668398f24d35c69d027ba3e4ca13a4a07eea822f
Hao Xiong <haoxiong@outlook.com>
2017-03-16 03:15:10 -0500

    Report the accuracy for 20 suitable combinations of number of iterations and the learning rate.

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -18,28 +18,45 @@ public class Perceptron {
     private static final String[] STOP_WORDS = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"};
     public static final int HAM = 0;
     public static final int SPAM = 1;
-    private static final String[] TRAIN_PATH = {"dataset/enron4/train/ham", "dataset/enron4/train/spam"};
-    private static final String[] TEST_PATH = {"dataset/enron4/test/ham", "dataset/enron4/test/spam"};
+    private static final String[] TRAIN_PATH = {"dataset/hw2/train/ham", "dataset/hw2/train/spam"};
+    private static final String[] TEST_PATH = {"dataset/hw2/test/ham", "dataset/hw2/test/spam"};
 
     private static List<String> stop_words;
     private static Map<String, Integer> dictionary;
     private static double[] W;
     private static int[] numberOfTestFiles = {0, 0};
+    private static double[] learningRates = new double[20];
+    private static int[] iterations = new int[20];
 
     //static initializer
     static {
         numberOfTestFiles[HAM] = new File(TEST_PATH[HAM]).list().length;
         numberOfTestFiles[SPAM] = new File(TEST_PATH[SPAM]).list().length;
+        for (int i = 0; i < 20; i++) {
+            learningRates[i] = 0.006 + 0.0001 * i;
+            iterations[i] = 30 + i;
+        }
         stop_words = Arrays.asList(STOP_WORDS);
     }
 
     public static void main(String[] args) {
-        double learningRate = 0.0071;
-        int iteration = 70;
-        boolean removeStopWords = false;
-        countWords(TRAIN_PATH, removeStopWords);
-        trainPerceptron(toVectors(TRAIN_PATH, removeStopWords), learningRate, iteration);
-        System.out.println("learningRate = " + learningRate + ", iteration = " + iteration + " => Accuracy: " + testAccuracy(toVectors(TEST_PATH, removeStopWords)));
+
+        System.out.println("---------------Keep Original Words---------------");
+        for (int i = 0; i < 20; i++) {
+            output(learningRates[i], iterations[i], false);
+        }
+
+        System.out.println("\n\n-----------Filtering Out The Stop Words----------");
+        for (int i = 0; i < 20; i++) {
+            output(learningRates[i], iterations[i], true);
+        }
+    }
+
+    private static void output(double learningRate, int iteration, boolean filter) {
+        countWords(TRAIN_PATH, filter);
+        trainPerceptron(toVectors(TRAIN_PATH, filter), learningRate, iteration);
+        System.out.println("================================================");
+        System.out.println("learningRate = " + learningRate + ", iteration = " + iteration + " => Accuracy: " + testAccuracy(toVectors(TEST_PATH, filter)));
     }
 
     /**
