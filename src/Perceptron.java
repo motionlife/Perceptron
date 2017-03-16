55787e0338110a0e3c9ee1ee1a45cceaad59a008
Hao Xiong <haoxiong@outlook.com>
2017-03-16 02:40:06 -0500

    Add more data sets

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -18,8 +18,8 @@ public class Perceptron {
     private static final String[] STOP_WORDS = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"};
     public static final int HAM = 0;
     public static final int SPAM = 1;
-    private static final String[] TRAIN_PATH = {"dataset/train/ham", "dataset/train/spam"};
-    private static final String[] TEST_PATH = {"dataset/test/ham", "dataset/test/spam"};
+    private static final String[] TRAIN_PATH = {"dataset/enron4/train/ham", "dataset/enron4/train/spam"};
+    private static final String[] TEST_PATH = {"dataset/enron4/test/ham", "dataset/enron4/test/spam"};
 
     private static List<String> stop_words;
     private static Map<String, Integer> dictionary;
@@ -34,13 +34,12 @@ public class Perceptron {
     }
 
     public static void main(String[] args) {
-        double learningRate = 0.001;
-        int iteration = 50;
+        double learningRate = 0.0071;
+        int iteration = 70;
         boolean removeStopWords = false;
         countWords(TRAIN_PATH, removeStopWords);
         trainPerceptron(toVectors(TRAIN_PATH, removeStopWords), learningRate, iteration);
-        System.out.println("learningRate = " + learningRate + ", iteration = " + iteration +
-                "\nAccuracy of perceptron algorithm: " + testAccuracy(toVectors(TEST_PATH, removeStopWords)));
+        System.out.println("learningRate = " + learningRate + ", iteration = " + iteration + " => Accuracy: " + testAccuracy(toVectors(TEST_PATH, removeStopWords)));
     }
 
     /**
@@ -123,10 +122,12 @@ public class Perceptron {
         W = new double[size];//initially set all w=0
         while (repeat-- > 0) {
             for (TextVector tv : vectors) {
-                int error =  tv.predictionError(W);
-                for (int i = 0; i < size; i++) {
-                    //update the parameters according to prediction error with respect to this single training example only.
-                    if (tv.features[i] != 0) W[i] += learningRate * tv.features[i] * error;
+                int error = tv.predictionError(W);
+                if (error != 0) {
+                    for (int i = 0; i < size; i++) {
+                        //update the parameters according to prediction error with respect to this single training example only.
+                        if (tv.features[i] != 0) W[i] += learningRate * tv.features[i] * error;
+                    }
                 }
             }
         }
@@ -138,7 +139,7 @@ public class Perceptron {
     private static double testAccuracy(ArrayList<TextVector> test_vectors) {
         int correct = 0;
         for (TextVector tv : test_vectors) {
-            if (tv.estimateType(W) == tv.type) correct++;
+            if (tv.predictionError(W) == 0) correct++;
         }
         return (double) correct / (numberOfTestFiles[HAM] + numberOfTestFiles[SPAM]);
     }
@@ -162,12 +163,11 @@ class TextVector {
         return type - estimateType(W);
     }
 
-    int estimateType(double[] W) {
+    private int estimateType(double[] W) {
         double sum = 0;
         for (int i = 0; i < W.length; i++) {
             if (features[i] != 0) sum += W[i] * features[i];
         }
         return sum > 0 ? 1 : -1;
     }
-
 }
